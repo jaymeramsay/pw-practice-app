1a4f82aa9361137df7495540edc7b02b00bef5ce
jaymeramsay <jayme.ramsay@gmail.com>
2026-03-26 22:12:13 -0500

    creates browser dialog test and web table tests with notes

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -79,6 +79,11 @@ test.describe("Modals and Overlays Page", () => {
         const tooltip = await page.locator('nb-tooltip').textContent(); //.innerText() might be a better option here depending on the situation.
         expect(tooltip).toEqual('This is a tooltip');
     })
+
+    test('browser dialog boxes', async ({ page }) => { 
+        await page.getByText('Dialog').click();
+    })
+
 });
 
 test('lists and dropdowns', async ({ page }) => {
@@ -110,3 +115,65 @@ test('lists and dropdowns', async ({ page }) => {
             await dropdownMenu.click();
     }
 })
+
+// part of the dialog box lesson but it lives on a different page than the Dialog section
+ test('browser dialog boxes', async ({ page }) => { 
+        await page.getByText('Tables & Data').click();
+        await page.getByText('Smart Table').click();
+
+        //create an event listenter
+        page.on('dialog', async dialog => {
+            expect(dialog.message()).toEqual('Are you sure you want to delete?'); 
+            dialog.accept();
+        })
+
+        await page.getByRole('table').locator('tr', {hasText: 'mdo@gmail.com'}).locator('nb-trash').click(); 
+        await expect(page.locator('table tr').first()).not.toHaveText('mdo@gmail.com');
+ });
+
+// this test shows how to specifically target a table row and then click on an element within that row. 
+// This is a common use case when working with tables.
+ test('web tables', async ({ page }) => {
+    await page.getByText('Tables & Data').click();
+    await page.getByText('Smart Table').click();
+
+    // 1 get this row by any test in this row
+    const targetRow = page.getByRole('row', { name: 'twitter@outlook.com' });
+    await targetRow.locator('nb-edit').click(); //clicks the edit button in targetRow
+    await targetRow.locator('input-editor').getByPlaceholder('Age').clear(); // since the 'twitter@outlook.com' is a
+    // property value and not html text, we had to use a different approach to grab the cell that we wanted to change the age input
+    await targetRow.locator('input-editor').getByPlaceholder('Age').fill('25'); 
+    await page.locator('nb-checkmark').click(); // clicks the checkmark to save the changes 
+
+    //2 get the row based on the value in a specific column 
+    await page.locator('.ng2-smart-pagination-nav').getByText('2').click(); // navigate to the second page of the table 
+    const targetRowByID = page.getByRole('row', { name: '11' }).filter({has: page.locator('td').nth(1).getByText('11')}); // this is an example of how to grab a 
+    // row based on the value in a specific column. In this case, we are grabbing the row where the value in the second column is '11'.
+    await targetRowByID.locator('.nb-edit').click(); 
+    await page.locator('input-editor').getByPlaceholder('Email').clear()
+    await page.locator('input-editor').getByPlaceholder('Email').fill('test@test.com')
+    await expect(targetRowByID.locator('td').nth(5)).toHaveText('test@test.com')
+
+    //3 test filter of the table
+    const ages = ['20', '30', '40', '200'] //test data
+
+    //created a loop to loop through each of the test data values
+    for(let age of ages){
+        await page.locator('input-filter').getByPlaceholder('Age').clear()
+        await page.locator('input-filter').getByPlaceholder('Age').fill(age)
+        await page.waitForTimeout(500) //this delay is created because playwright was running faster than the page could load
+        const ageRows = page.locator('tbody tr')
+
+        // created a second loop to loop through age values and assert the values match
+        for(let row of await ageRows.all()){
+            const cellValue = await row.locator('td').last().textContent();
+            if(age = "200"){
+                expect(await page.getByRole('table').textContent()).toContain('No Data Found')
+            } else {
+                expect(cellValue).toEqual(age)
+            }
+            expect(cellValue).toEqual(age);
+        }
+    }
+
+ })
